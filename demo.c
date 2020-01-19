ecac9af7a9487e65d189b6467ec3bfaae5873f69
Vikas <vikasgupta358@gmail.com>
2020-01-20 00:00:12 +0530

    first commmit

---
+++ demo.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+int first_module_init(void)
+{
+	printk(KERN_ALERT "Hey World Whats up?\n\n");
+	return 0;
+}
+
+void first_module_exit(void)
+{
+	printk(KERN_ALERT "Leaving this World\n\n");
+	return 0;
+	
+	
+}
+
+module_init(first_module_init);
+module_exit(first_module_exit);
